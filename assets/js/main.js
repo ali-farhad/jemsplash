49a86b03ec0eef07d99d7aa9ca2bfe0183a2fcc8
ali-farhad <bc170400041@vu.edu.pk>
2020-12-26 02:54:52 +0500

    updated mobile-nav on pricing + course_preview pages

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+ 
+    
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+
+
+
+
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+function close(){
+    mainMenu.style.top = '-100%';
+}
+
+
+  });
